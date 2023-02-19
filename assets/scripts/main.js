08d7d8b701ff52ff8a8dc0dc7295c7812971e4ba
Robert Frampton <rob.g.frampton@gmail.com>
2023-02-19 18:04:52 -0500

    Animation tweaks

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,8 +7,8 @@ const loader = new THREE.GLTFLoader();
 
 const SHAPES = [
   {
-    name: 'brackets',
-    color: 0x0EB1D2,
+    name: "brackets",
+    color: 0x0eb1d2,
     delay: 100,
     position: new THREE.Vector3(0.3, 0.1, 4.2),
     rotation: {
@@ -22,11 +22,11 @@ const SHAPES = [
         y: -0.1,
         z: -0.1,
       },
-    }
+    },
   },
   {
-    name: 'git',
-    color: 0xF96E46,
+    name: "git",
+    color: 0xf96e46,
     delay: 300,
     position: new THREE.Vector3(-0.6, -0.2, 4),
     rotation: {
@@ -40,11 +40,11 @@ const SHAPES = [
         y: -0.1,
         z: 0.1,
       },
-    }
+    },
   },
   {
-    name: 'database',
-    color: 0xFCCA46,
+    name: "database",
+    color: 0xfcca46,
     delay: 500,
     position: new THREE.Vector3(-0.8, 0.6, 3),
     rotation: {
@@ -58,11 +58,11 @@ const SHAPES = [
         y: 0.1,
         z: -0.1,
       },
-    }
+    },
   },
   {
-    name: 'stack',
-    color: 0x399E5A,
+    name: "stack",
+    color: 0x399e5a,
     delay: 500,
     position: new THREE.Vector3(1.4, -0.8, 3),
     rotation: {
@@ -76,11 +76,11 @@ const SHAPES = [
         y: -0.1,
         z: 0.1,
       },
-    }
+    },
   },
   {
-    name: 'terminal',
-    color: 0xA964CE,
+    name: "terminal",
+    color: 0xa964ce,
     delay: 600,
     position: new THREE.Vector3(0.1, -1.5, 2),
     rotation: {
@@ -94,10 +94,10 @@ const SHAPES = [
         y: 0.1,
         z: 0.1,
       },
-    }
+    },
   },
   {
-    name: 'cloud',
+    name: "cloud",
     color: 0x345995,
     delay: 800,
     position: new THREE.Vector3(-3, 1.5, 2),
@@ -112,10 +112,10 @@ const SHAPES = [
         y: -0.1,
         z: -0.1,
       },
-    }
+    },
   },
   {
-    name: 'window',
+    name: "window",
     color: 0x394648,
     delay: 100,
     position: new THREE.Vector3(3.5, 2.5, 1),
@@ -130,34 +130,28 @@ const SHAPES = [
         y: 0.1,
         z: -0.1,
       },
-    }
+    },
   },
-]
+];
 
 const randomNumberFromInterval = (min, max) =>
   Math.random() * (max - min) + min;
 
-const wait = (ms = 0) =>
-  new Promise(resolve => setTimeout(resolve, ms))
+const wait = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const initScene = () => {
   const width = window.innerWidth - 300;
   const height = window.innerHeight;
 
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    width / height,
-    0.1,
-    1000
-  );
+  const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
   const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 
   renderer.setSize(width, height);
   renderer.setPixelRatio(2);
   document.body.appendChild(renderer.domElement);
-  renderer.domElement.className = 'float-right'
+  renderer.domElement.className = "float-right";
 
   camera.position.z = 5;
 
@@ -187,7 +181,7 @@ const initLights = (scene) => {
 const initShape = async (scene, shapeSettings) => {
   const { name, position, delay, color, rotation } = shapeSettings;
 
-  await wait(delay)
+  await wait(delay);
 
   return new Promise((resolve, reject) => {
     loader.load(
@@ -202,8 +196,8 @@ const initShape = async (scene, shapeSettings) => {
           }
         });
 
-        new TWEEN.Tween({opacity: 0})
-          .to( { opacity: 1 }, 1000)
+        new TWEEN.Tween({ opacity: 0 })
+          .to({ opacity: 1 }, 1000)
           .easing(TWEEN.Easing.Quartic.In)
           .onUpdate(({ opacity }) => {
             mesh.traverse((obj) => {
@@ -218,17 +212,29 @@ const initShape = async (scene, shapeSettings) => {
 
         const group = new THREE.Group();
 
-        group.add(mesh);
+        // Start position
+        Object.assign(group.position, { ...position, z: position.z - 0.4 });
 
-        Object.assign(group.position, position);
+        console.log(group.position)
+
+        // Animate position
+        new TWEEN.Tween(group.position)
+          .to(position, 1000)
+          .easing(TWEEN.Easing.Cubic.Out)
+          .start();
+
+        // Start rotation
         Object.assign(group.rotation, rotation.start);
 
+        // Animate rotation
         new TWEEN.Tween(group.rotation)
           .to(rotation.end, randomNumberFromInterval(4000, 8000))
           .yoyo(true)
           .repeat(Infinity)
           .easing(TWEEN.Easing.Sinusoidal.InOut)
           .start();
+
+        group.add(mesh);
 
         // Centers loaded shape
         new THREE.Box3()
@@ -251,11 +257,7 @@ const bootstrap = async () => {
 
   initLights(scene);
 
-  await Promise.all(
-    SHAPES.map(
-      (shape) => initShape(scene, shape)
-    )
-  );
+  await Promise.all(SHAPES.map((shape) => initShape(scene, shape)));
 
   const animate = () => {
     requestAnimationFrame(animate);
